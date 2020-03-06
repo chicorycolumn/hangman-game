f54a084e692e7e5f6ad01ed6e8cdbe9800c11119
juditlehoczki <lehoczki.judit@gmail.com>
2020-03-06 15:04:20 +0000

    half way there

--- src/App.js
+++ src/App.js
@@ -5,57 +5,96 @@
 // Undo last move.
 // Add pictures.
 
-import React from "react";
-import "./App.css";
-import hang1 from "../hm/hang1";
+import React from 'react';
+import './App.css';
+import hang9 from './hm/hang1.png';
+import hang8 from './hm/hang2.png';
+import hang7 from './hm/hang3.png';
+import hang6 from './hm/hang4.png';
+import hang5 from './hm/hang5.png';
+import hang4 from './hm/hang6.png';
+import hang3 from './hm/hang7.png';
+import hang2 from './hm/hang8.png';
+import hang1 from './hm/hang9.png';
+import hang0 from './hm/hangX.png';
 
 class App extends React.Component {
   state = {
-    hiddenWord: "SEVEN",
-    displayWord: "_____",
-    guessesRemaining: 10
+    hiddenWord: 'SEVEN',
+    displayWord: '',
+    guessesRemaining: 9,
+    imagePicker: {
+      9: hang9,
+      8: hang8,
+      7: hang7,
+      6: hang6,
+      5: hang5,
+      4: hang4,
+      3: hang3,
+      2: hang2,
+      1: hang1,
+      0: hang0
+    },
+    statusMsg: 'Good Luck!'
   };
 
   handleKeyPress = event => {
-    this.setState(currentState => {
-      if (currentState.hiddenWord.toLowerCase().includes(event.key)) {
-        let newWordToDisplay = currentState.hiddenWord
-          .toLowerCase()
-          .split("")
-          .map((letter, index) => {
-            if (letter.toLowerCase() === event.key) {
-              return letter;
-            } else {
-              if (currentState.displayWord[index] !== "_") {
-                return currentState.displayWord[index];
-              } else return "_";
-            }
-          });
+    if (this.state.guessesRemaining >= 0) {
+      this.setState(currentState => {
+        if (currentState.hiddenWord.toLowerCase().includes(event.key)) {
+          let newWordToDisplay = currentState.hiddenWord
+            .toLowerCase()
+            .split('')
+            .map((letter, index) => {
+              if (letter.toLowerCase() === event.key) {
+                return letter;
+              } else {
+                if (currentState.displayWord[index] !== '_') {
+                  return currentState.displayWord[index];
+                } else return '_';
+              }
+            });
 
-        return { displayWord: newWordToDisplay.join("") };
-      } else {
-        return { guessesRemaining: currentState.guessesRemaining - 1 };
-      }
-    });
+          return { displayWord: newWordToDisplay.join('') };
+        } else {
+          return this.state.guessesRemaining === 0
+            ? { statusMsg: 'You lost.' }
+            : {
+                guessesRemaining: currentState.guessesRemaining - 1,
+                statusMsg: `You have ${this.state.guessesRemaining} guesses remaining! `
+              };
+        }
+      });
+    }
   };
 
   componentDidMount() {
-    document.addEventListener("keydown", this.handleKeyPress);
+    document.addEventListener('keydown', this.handleKeyPress);
+    this.setState(currentState => {
+      const populateDisplayWord = () => {
+        return '_'.repeat(currentState.hiddenWord.length);
+      };
+      return { displayWord: populateDisplayWord() };
+    });
   }
 
   render() {
+    console.log(`hang${this.state.guessesRemaining}`);
     return (
       <div className="App">
         <header className="Manhang"></header>
         <p>Hello and welcome to Han that Mang!</p>
         <p>
           {this.state.displayWord
-            .split("")
-            .join(" ")
+            .split('')
+            .join(' ')
             .toUpperCase()}
         </p>
-        <img src="./hm/hang1.png" alt="hangman stage 1"></img>
-        <p>You have {this.state.guessesRemaining} guesses remaining!</p>
+        <img
+          src={this.state.imagePicker[this.state.guessesRemaining]}
+          alt="hangman stage 1"
+        ></img>
+        <p>{this.state.statusMsg}</p>
       </div>
     );
   }
