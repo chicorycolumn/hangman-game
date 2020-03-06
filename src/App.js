52be284c96dee3e6cf049a022150a2abddfc3697
C <chrismatusemail@gmail.com>
2020-03-06 14:07:10 +0000

    added images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+// You won, when displayWord no longer contains any underscores.
+// You lost, when remainingGuesses <= 0.
+// New Game button, picks new words from array of words at random. Easy Medium Hard arrays.
+// Save Button - save the state to local storage, and we add to mount fxn loading state from local storage.
+// Undo last move.
+// Add pictures.
+
 import React from "react";
 import "./App.css";
+import hang1 from "../hm/hang1";
 
 class App extends React.Component {
   state = {
@@ -46,6 +54,7 @@ class App extends React.Component {
             .join(" ")
             .toUpperCase()}
         </p>
+        <img src="./hm/hang1.png" alt="hangman stage 1"></img>
         <p>You have {this.state.guessesRemaining} guesses remaining!</p>
       </div>
     );
